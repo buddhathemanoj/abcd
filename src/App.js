7a4f010e5ba130de451752e7dfec137ee1682e84
yeskay1011 <mrsaravanan1012@gmail.com>
2023-11-16 18:38:40 +0530

    changes in addpermit and components created

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthGuard from './Auth/authgaurd';
 import Login from './Login/Login';
 import Dashboard from './Dashboard/Dashboard';
+import Sites from './Pages/Sites/Sites';
+import Maps from './Pages/Maps/Maps';
 import Users from './Users/Users';
 import Permit from './Pages/Permit/Permit';
 import Account from './Pages/Accounts/Account';
 import AddPermit from './Pages/Permit/AddPermit';
+import Templates from './Pages/AllTemplates/Templates';
+import FormsDownlod from './Pages/FormsDownload/FormsDownlod';
+import FeedbackForm from './Pages/FeedbackForm/FeedbackForm';
 
 function App() {
   return (
@@ -15,11 +20,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<AuthGuard component={<Dashboard />} />} />
+          <Route path="/sites" element={<AuthGuard component={<Sites />} />} />
+          <Route path="/maps" element={<AuthGuard component={<Maps />} />} />
           <Route path="/users" element={<AuthGuard component={<Users />} />} />
           <Route path="/all-permits" element={<AuthGuard component={<Permit />} />} />
           <Route path="/all-permits-create" element={<AuthGuard component={<AddPermit />} />} />
-
+          <Route path="/all-templates" element={<AuthGuard component={<Templates />} />} />
           <Route path="/account" element={<AuthGuard component={<Account />} />} />
+          <Route path="/forms-download" element={<AuthGuard component={<FormsDownlod />} />} />
+          <Route path="/feedback-form" element={<AuthGuard component={<FeedbackForm />} />} />
         </Routes>
       </Router>
     </div>
---
+++ src/Pages/Sites/Sites.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sites = () => {
+    return (
+        <div className='d-flex justify-content-center align-items-center vh-100'>
+            <h2>Processing...</h2>
+        </div>
+    )
+}
+
+export default Sites

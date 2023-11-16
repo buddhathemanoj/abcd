de149fe149b2b5cfd74f9ad886f12808c7167c4f
AshokKumar185 <ashokkumar936127@gmail.com>
2023-11-16 06:57:54 +0530

    New

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,16 +1,91 @@
 import React from "react";
 import { connect } from "react-redux";
+import "./Dashboard.css"
+import vector from "../Asset/noto_hourglass-with-flowing-sand.png"
+import { RiArrowDropDownLine } from "react-icons/ri";
+
+const workArr = [
+    "General Permit To Work(3)","A1 - Hot Works(5)","A2 - Confied Space(7)","A3 - Lifting Space(0)","A4 - Work at Height(3)","Admin login successful"
+]
+
 const Dashboard = (state) => {
-    
-return(
-    <div>
-<h1>this is dashboard</h1>
-        
-    </div>
-)
+
+    return (
+        <div className="dashboard-main-container">
+            <h1 className="welcome-heading">Welcome To JCET,</h1>
+            <ul className="dashboard-list-container">
+                <li className="dashboard-list-item-container">
+                    <div className="image-active-container">
+                        <div className="vector-img-container">
+                            <img src={vector} alt="vector" className="vector-img" />
+                        </div>
+                        <div>
+                            <p className="total-desc">Total Active Permits</p>
+                            <p className="general-desc">General Permit To Work</p>
+                            <h1 style={{textAlign:"center",marginTop:"5px",fontSize:"55px",color:"#022088",marginBottom:"0px"}}>3</h1>
+                        </div>
+                    </div>
+                    <div className="view-btn-container">
+                        <button type="button" className="view-btn">View</button>
+                    </div>
+                </li>
+                <li className="dashboard-list-item-container">
+                    <div className="image-active-container">
+                        <div className="vector-img-container2">
+                            <img src={vector} alt="vector" className="vector-img" />
+                        </div>
+                        <div>
+                            <p className="total-desc">Total Active Permits</p>
+                            <p className="general-desc">Hot works</p>
+                            <h1 style={{textAlign:"center",marginTop:"5px",fontSize:"55px",color:"#022088",marginBottom:"0px"}}>5</h1>
+                        </div>
+                    </div>
+                    <div className="view-btn-container">
+                        <button type="button" className="view-btn">View</button>
+                    </div>
+                </li>
+                <li className="dashboard-list-item-container">
+                    <div className="image-active-container">
+                        <div className="vector-img-container3">
+                            <img src={vector} alt="vector" className="vector-img" />
+                        </div>
+                        <div>
+                            <p className="total-desc">Total Active Permits</p>
+                            <p className="general-desc">Lifting Space</p>
+                            <h1 style={{textAlign:"center",marginTop:"5px",fontSize:"55px",color:"#022088",marginBottom:"0px"}}>7</h1>
+                        </div>
+                    </div>
+                    <div className="view-btn-container">
+                        <button type="button" className="view-btn">View</button>
+                    </div>
+                </li>
+                <li className="dashboard-list-item-container">
+                    <div className="image-active-container">
+                        <div className="vector-img-container4">
+                            <img src={vector} alt="vector" className="vector-img" />
+                        </div>
+                        <div>
+                            <p className="total-desc">Total Active Permits</p>
+                            <p className="general-desc">Work at height</p>
+                            <h1 style={{textAlign:"center",marginTop:"5px",fontSize:"55px",color:"#022088",marginBottom:"0px"}}>3</h1>
+                        </div>
+                    </div>
+                    <div className="view-btn-container">
+                        <button type="button" className="view-btn">View</button>
+                    </div>
+                </li>
+            </ul>
+            <ul className="work-list-container">
+                {workArr.map((eachWork) => <li className="work-list-item-container">
+                    <p style={{fontSize:"20px",color:"#000000",margin:"0px"}}>{eachWork}</p>
+                    <button type="button" className="drop-btn"><RiArrowDropDownLine /></button>
+                </li>)}
+            </ul>
+        </div>
+    )
 }
 
 const mapStateToProps = (state) => ({
-    user:state.auth.user,
-   })
-    export default connect(mapStateToProps)(Dashboard);
+    user: state.auth.user,
+})
+export default connect(mapStateToProps)(Dashboard);

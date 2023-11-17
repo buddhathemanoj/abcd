272ac457ed7336f0b47d891a46f1e07e5ac8afa9
AshokKumar185 <ashokkumar936127@gmail.com>
2023-11-17 17:33:17 +0530

    apa

--- src/Components/UserSidebar.js
+++ src/Components/UserSidebar.js
@@ -36,7 +36,7 @@ const UserSidebar = () => {
       <NavLink to="/user-dashboard"  className={isClick === 1 ? "active-tap" : ""} onClick={() => clickToSet(1)}> <FontAwesomeIcon style={{marginRight:'5px'}} icon={faHome} />Dashboard</NavLink>
       <NavLink to="/my-permits"  className={isClick === 2 ? "active-tap" : ""} onClick={() => clickToSet(2)}><FaWpforms style={{marginRight:'5px'}} />My Permits</NavLink>
       <NavLink to="/my-child-permit"  className={isClick === 3 ? "active-tap" : ""} onClick={() => clickToSet(3)}><FaWpforms style={{marginRight:'5px'}} />My Child Permits</NavLink>
-      <NavLink to="/all-templates"  className={isClick === 4 ? "active-tap" : ""} onClick={() => clickToSet(4)}><CgTemplate style={{marginRight:'5px'}} />All Templates</NavLink>
+      <NavLink to="/all-template"  className={isClick === 4 ? "active-tap" : ""} onClick={() => clickToSet(4)}><CgTemplate style={{marginRight:'5px'}} />All Templates</NavLink>
       <NavLink to="/account"  className={isClick === 5 ? "active-tap" : ""} onClick={() => clickToSet(5)}><MdOutlineManageAccounts style={{marginRight:'5px'}} />Account</NavLink>
       <NavLink to="/forms-download"  className={isClick === 6 ? "active-tap" : ""} onClick={() => clickToSet(6)}><CiSaveDown2  style={{marginRight:'5px'}}/>Forms Download</NavLink>
       <NavLink to="/feedback-form"  className={isClick === 7 ? "active-tap" : ""} onClick={() => clickToSet(7)}><MdFeedback style={{marginRight:'5px'}} />Feedback Form</NavLink>
